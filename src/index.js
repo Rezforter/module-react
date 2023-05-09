5a33c6847675a60525942ac66e44a69074ffb169
Rezforter <rezforter@gmail.com>
2023-05-09 03:15:10 +0300

    changes in package4

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import AuthRegForm, { activeUserDataKey } from './pages/authRegForm';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <AuthRegForm />
@@ -33,7 +33,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter router={router} />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
